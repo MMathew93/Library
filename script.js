3f4c16f5337f5017fa4e06a22b3fe50d06990064
lyndexer <mckinney.m93@gmail.com>
2020-04-20 18:38:05 -0700

    starting the javascript

--- script.js
+++ script.js
@@ -1,13 +1,27 @@
-function Book(title, author, pages, read) {
-    this._title= title;
-    this._author= author;
-    this._pages= pages;
-    this._read= true;
-    info() {
-      return `The ${this._title} by ${this._author}, ${this._pages} pages, ${this._read}`
+let myLibrary= [];
+
+class Book {
+    constructor (title, author, pages, read) {
+      this._title= title;
+      this._author= author;
+      this._pages= pages;
+      this._read= true;
+      }
+      
+      info() {
+        return `${this._title} by ${this._author}, ${this._pages} pages, ${this._read}`
+      }
     }
+    
+    
+    const hobbit= new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
+    console.log(hobbit.info())
+
+
+
+
+
+    
+  function addBookToLibrary() {
+
   }
-  
-  
-  const hobbit= new Book('The Hobbit', 'J.R.R. Tolkien', 295, false)
-  console.log(hobbit.info)
